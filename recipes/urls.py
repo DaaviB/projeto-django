93732d55d3bf439ed114fbaa9e5aeb695a00c767
Davi Brito <britodavi122@gmail.com>
2022-04-07 07:17:00 -0300

    testing urls and views functions

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("recipe/category/<int:category_id>/",
+    path("recipes/category/<int:category_id>/",
          views.category, name='category'
          ),
     path("recipe/<int:id>/", views.recipe, name='recipe'),
