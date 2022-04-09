0fbbb18060b04b325fe683fdf2086e981b6e32b5
Davi Brito <britodavi122@gmail.com>
2022-04-09 20:26:14 -0300

    methods for category, author and recipes. PS: skip and self.fail.

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,9 +5,6 @@ from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
-    def tearDown(self):
-        return super().tearDown()
-
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -28,13 +25,12 @@ class RecipeViewsTest(RecipeTestBase):
         )
 
     def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         context = response.context['recipes']
 
         self.assertIn('Recipe Title', content)
-        self.assertIn('5 Pessoas', content)
-        self.assertIn('10 Minutos', content)
         self.assertEqual(len(context), 1)
 
     def test_recipe_category_view_function_is_correct(self):
