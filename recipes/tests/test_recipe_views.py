67d939ffd218385f53028875acd828b82ead60ff
Davi Brito <britodavi122@gmail.com>
2022-04-10 20:01:03 -0300

    + tests for views

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -65,6 +65,17 @@ class RecipeViewsTest(RecipeTestBase):
 
         self.assertIn(needed_title, content)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        # need a recipe for test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={
+                    'category_id': recipe.category.id})
+        )
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
@@ -89,3 +100,16 @@ class RecipeViewsTest(RecipeTestBase):
         )
         content = response.content.decode('utf-8')
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        # need a recipe for test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe', kwargs={
+                    'id': recipe.id
+                }
+            )
+        )
+        self.assertEqual(response.status_code, 404)
