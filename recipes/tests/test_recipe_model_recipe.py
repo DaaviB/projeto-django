e006424ab18d5c1fd86eb52ca0cfd1a919a3c1a0
Davi Brito <britodavi122@gmail.com>
2022-04-12 09:28:50 -0300

    coverage, test model recipe and category

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -50,3 +50,12 @@ class RecipeModelTest(RecipeTestBase):
     def test_recipe_is_published_is_false_by_default(self):
         recipe = self.make_recipe_no_defaults()
         self.assertFalse(recipe.is_published)
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), needed)
+
+        ...
