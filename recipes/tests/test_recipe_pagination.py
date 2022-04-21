bf1cd76f6de8315d437f0ecf859a0c1f2ac14592
DaviBrito <britodavi122@gmail.com>
2022-04-21 18:21:47 -0300

    fixed test for pagination

--- recipes/tests/test_recipe_pagination.py
+++ recipes/tests/test_recipe_pagination.py
@@ -1,5 +1,4 @@
 from django.http import HttpRequest
-from django.urls import reverse
 from utils.pagination import make_pagination
 
 from .test_recipe_base import RecipeTestBase
@@ -8,17 +7,17 @@ from .test_recipe_base import RecipeTestBase
 class MakePaginationTest(RecipeTestBase):
     def test_make_pagination_shows_correct_number_of_recipe_per_page(self):  # noqa: E501
 
-        for ind in range(1, 21):
-            self.make_recipe(
-                slug=f'slug-the-number{ind}',
-                author_data={
-                    'username': f'User{ind}',
-
-                }
-            )
-        url = reverse('recipes:home')
-        response = self.client.get(url)
-        self.assertEqual(9, response.context['recipes'].paginator.per_page)
+        request = HttpRequest()
+        request.GET = {'page': 1}
+        page_obj, pagination = make_pagination(
+            request,
+            range(1, 21),
+            1
+        )
+        self.assertEqual(
+            1,
+            page_obj.paginator.per_page,
+        )
 
     def test_make_pagination_raises_value_error_exception(self):
         request = HttpRequest()
