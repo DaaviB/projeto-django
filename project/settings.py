bb8ee49cf8a71700bd75d958fdc7997643a83623
DaviBrito <britodavi122@gmail.com>
2022-04-22 23:27:09 -0300

    created new app "authors"

--- project/settings.py
+++ project/settings.py
@@ -35,13 +35,14 @@ ALLOWED_HOSTS: List[str] = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
