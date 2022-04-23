677e655ee19fb5a98c7be650e52b8c815b4eceb4
DaviBrito <britodavi122@gmail.com>
2022-04-23 15:39:01 -0300

    form style

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,31 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        # exclude = [...]
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The email must be valid'
+        }
+
+        error_messages = {
+            'required': 'This field must not be empty'
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            })
+
+        }
