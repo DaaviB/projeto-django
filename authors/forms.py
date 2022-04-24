f9160ad6000917dc237a9b489764c31223a8f976
DaviBrito <britodavi122@gmail.com>
2022-04-24 09:42:25 -0300

    saving form data

--- authors/forms.py
+++ authors/forms.py
@@ -84,7 +84,6 @@ class RegisterForm(forms.ModelForm):
         }
         widgets = {
             'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your username here',
                 'class': 'input text-input'
             }),
             'password': forms.PasswordInput(attrs={
