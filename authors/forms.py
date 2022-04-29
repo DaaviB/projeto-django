21b66ad349a0e944a763e16ad28de01c5af19935
DaviBrito <britodavi122@gmail.com>
2022-04-29 10:43:24 -0300

    email form field validation

--- authors/forms.py
+++ authors/forms.py
@@ -101,11 +101,17 @@ class RegisterForm(forms.ModelForm):
             'username',
             'email',
         ]
-        error_messages = {
-            'username': {
-                'required': 'This field must not be empty',
-            }
-        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use',
+                code='invalid'
+            )
+        return email
 
     def clean(self):
         cleaned_bata = super().clean()
