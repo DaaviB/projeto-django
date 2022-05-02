840f1495efa9b35853c784a94ee3dfe31e1ac346
DaviBrito <britodavi122@gmail.com>
2022-05-02 17:00:38 -0300

    functional search test

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,5 +1,8 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from .base import RecipeBaseFunctionalTest
 
@@ -15,4 +18,32 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         body = browser.find_element(By.TAG_NAME, 'body')
         self.assertIn(
             'No recipes available to display at the moment', body.text
+        )
+
+    @patch('recipes.views.PER_PAGE', new=4)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This os what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê um campo de busca com o texto 'Search for recipes'
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for recipes"]'
+        )
+
+        #  Clica neste input e digita o termo de busca
+        #  para encontrar a receita com o título desejado
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        #  Usuário encontra o que desejava na página
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text
         )
