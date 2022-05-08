a3cce6334dadf289640d4e0556f148a9d2478eec
DaviBrito <britodavi122@gmail.com>
2022-05-07 22:16:51 -0300

    editing admin area

--- recipes/admin.py
+++ recipes/admin.py
@@ -10,8 +10,22 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-
-    ...
+    list_display = ('id', 'author', 'title', 'created_at', 'is_published',)
+    list_display_links = ('title', 'created_at',)
+    search_fields = (
+        'id', 'title', 'slug', 'created_at',
+        'description', 'preparation_steps',
+    )
+    list_filter = (
+        'category', 'author', 'created_at',
+        'update_at', 'is_published',
+    )
+    list_per_page = 10
+    list_editable = ('is_published',)
+    ordering = ('-id',)
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
 
 
 admin.site.register(Category, CategoryAdmin)
