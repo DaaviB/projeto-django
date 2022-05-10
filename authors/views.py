262c222076d466b41938b0ecf971f585b1fde6db
DaviBrito <britodavi122@gmail.com>
2022-05-10 18:03:44 -0300

    area for the user to create his recipes

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,10 @@
+
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 from recipes.models import Recipe
 
@@ -129,4 +131,36 @@ def dashboard_recipe_edit(request, id):
     return render(request, 'authors/pages/dashboard_recipe.html', context={
         'form': form,
         'recipe': recipe,
+    })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if request.POST:
+        if form.is_valid():
+            recipe: Recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.preparation_steps_is_html = False
+            recipe.is_published = False
+            recipe.slug = slugify(recipe.title)
+
+            recipe.save()
+            messages.success(request, 'Sua receita foi criada com sucesso.')
+            return redirect(reverse('authors:dashboard'))
+        else:
+            form = AuthorRecipeForm(request.POST or None)
+            messages.error(
+                request,
+                'Ocorreu algum erro na criação da sua receita, por favor, '
+                'corrija os campos incorretos.'
+            )
+
+    return render(request, 'authors/pages/dashboard_recipe.html', {
+        'form': form,
+        'form_action': reverse('authors:dashboard_recipe_new'),
     })
