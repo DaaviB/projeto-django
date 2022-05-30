9e79db1dc143981c8e0347f790b979ad57ffecc3
daavib <britodavi122@gamil.com>
2022-05-30 09:19:10 -0300

    profile are create when new user is registed

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = 'id', 'author', 'bio'
+    list_display_links = 'id', 'author',
+    ordering = '-id',
